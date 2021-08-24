8b70bc2469e93f77238307a8a8de7b698dc644ae
Iris Hsu <ishiotsuki@gmail.com>
2021-08-23 21:28:04 -0700

    heroku debugging

--- src/scenes/Mainscene.js
+++ src/scenes/Mainscene.js
@@ -1,41 +1,41 @@
-import Phaser from 'phaser'
-import io from 'socket.io-client';
-
-
+import Phaser from "phaser";
+import io from "socket.io-client";
+// updated this filename
 
 export default class MainScene extends Phaser.Scene {
-    constructor() {
-        super('MainScene'),
-            this.state = {
-                inRoom: false
-            };
+  constructor() {
+    super("MainScene"),
+      (this.state = {
+        inRoom: false,
+      });
+  }
 
-    }
+  preload() {
+    this.load.image("bg", "/assets/bg.jpg");
+  }
 
-    preload() {
-        this.load.image("bg", "/assets/bg.jpg")
-    }
+  create() {
+    //Add the background & fit to game window:
+    let image = this.add.image(
+      this.cameras.main.width / 2,
+      this.cameras.main.height / 2,
+      "bg"
+    );
+    let scaleX = this.cameras.main.width / image.width;
+    let scaleY = this.cameras.main.height / image.height;
+    let scale = Math.max(scaleX, scaleY);
+    image.setScale(scale).setScrollFactor(0);
 
-    create() {
-        //Add the background & fit to game window:
-        let image = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'bg')
-        let scaleX = this.cameras.main.width / image.width
-        let scaleY = this.cameras.main.height / image.height
-        let scale = Math.max(scaleX, scaleY)
-        image.setScale(scale).setScrollFactor(0)
+    let scene = this;
 
-        let scene = this;
+    //create socket:
+    this.socket = io();
 
-        //create socket:
-        this.socket = io()
+    //LAUNCH WAITING ROOM:
+    scene.scene.launch("Login", { socket: scene.socket });
+  }
 
-        //LAUNCH WAITING ROOM:
-        scene.scene.launch('Login', { socket: scene.socket })
-
-    }
-
-
-    update() {
-        const scene = this
-    }
+  update() {
+    const scene = this;
+  }
 }
