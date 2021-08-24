94525f91439e121a44ab6a45abc061599c28ad1d
Tika Llyn <tikallyn@gmail.com>
2021-08-24 16:21:43 -0500

    feat: added splash intro page with login & music links

--- src/scenes/Mainscene.js
+++ src/scenes/Mainscene.js
@@ -13,12 +13,14 @@ export default class MainScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image("bg", "/assets/bg.jpg")
+        this.load.image("logo", "/assets/meow-logo.jpg")
+        this.load.image("loginbutton", "/assets/elements/button_login_signUp.png")
+        this.load.image("meowsicbutton", "/assets/elements/button_makeMeowsic.png")
     }
 
     create() {
         //Add the background & fit to game window:
-        let image = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'bg')
+        let image = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'logo')
         let scaleX = this.cameras.main.width / image.width
         let scaleY = this.cameras.main.height / image.height
         let scale = Math.max(scaleX, scaleY)
@@ -29,8 +31,19 @@ export default class MainScene extends Phaser.Scene {
         //create socket:
         this.socket = io()
 
+        //BUTTONS!!
+        this.meowsicButton = this.add.image(700, 550, 'meowsicbutton').setScale(.8, .8).setInteractive();
+        this.meowsicButton.on("pointerdown", function () {
+            //LATER CHANGE TO WAITINGROOM SCREEN!!
+            scene.scene.launch('MeowsicRoom', { socket: scene.socket })
+        })
+        this.loginButton = this.add.image(300, 550, 'loginbutton').setScale(.8, .8).setInteractive();
+        this.loginButton.on("pointerdown", function () {
+            scene.scene.launch('Login', { socket: scene.socket })
+        })
+
         //LAUNCH WAITING ROOM:
-        scene.scene.launch('Login', { socket: scene.socket })
+        // scene.scene.launch('Login', { socket: scene.socket })
 
     }
 
