2aa364e774be551653bda306cc29a5f5cd322713
Iris Hsu <ishiotsuki@gmail.com>
2021-08-27 12:35:32 -0700

    updated zone and menu models to remove outlines

--- client/helpers/menu.js
+++ client/helpers/menu.js
@@ -7,15 +7,5 @@ export default class Menu {
       dropZone.setData({ isMenu: true });
       return dropZone;
     };
-    this.renderOutline = (dropZone) => {
-      let dropZoneOutline = scene.add.graphics();
-      dropZoneOutline.lineStyle(4, 0xff69b4);
-      dropZoneOutline.strokeRect(
-        dropZone.x - dropZone.input.hitArea.width / 2,
-        dropZone.y - dropZone.input.hitArea.height / 2,
-        dropZone.input.hitArea.width,
-        dropZone.input.hitArea.height
-      );
-    };
   }
 }
--- client/helpers/zone.js
+++ client/helpers/zone.js
@@ -7,15 +7,5 @@ export default class Zone {
       dropZone.setData({ occupied: false, isMenu: false });
       return dropZone;
     };
-    this.renderOutline = (dropZone) => {
-      let dropZoneOutline = scene.add.graphics();
-      dropZoneOutline.lineStyle(4, 0xff69b4);
-      dropZoneOutline.strokeRect(
-        dropZone.x - dropZone.input.hitArea.width / 2,
-        dropZone.y - dropZone.input.hitArea.height / 2,
-        dropZone.input.hitArea.width,
-        dropZone.input.hitArea.height
-      );
-    };
   }
 }
