8d02dd494aff5ad5ca8062f2201a1f7430beff34
Jane Balfus <janebalfus@gmail.com>
2021-09-02 15:56:40 -0400

    User name appears in Meowsic room

--- client/scenes/WaitingRoom.js
+++ client/scenes/WaitingRoom.js
@@ -26,7 +26,7 @@ export default class WaitingRoom extends Phaser.Scene {
 
         scene.popUp = scene.add.graphics();
 
-        console.log(this.state.loggedInUser);
+        console.log(scene.state.loggedInUser);
 
         // for popup window
         // scene.popUp.lineStyle(1, 0xffffff);
@@ -108,7 +108,7 @@ export default class WaitingRoom extends Phaser.Scene {
             scene.state.numPlayers = numPlayers;
             scene.state.inRoom = true;
             scene.state.placedCats = placedCats;
-            scene.scene.start("MeowsicRoom", { ...scene.state, socket: scene.socket, username: scene.username });
+            scene.scene.start("MeowsicRoom", { ...scene.state, socket: scene.socket, user: scene.state.loggedInUser });
 
         })
     }
