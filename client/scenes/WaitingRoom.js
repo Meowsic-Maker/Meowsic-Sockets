eee3c9d182da6fe8415359a8c8fb4c2481304b1b
Iris Hsu <ishiotsuki@gmail.com>
2021-09-02 16:21:09 -0700

    store uusernames in state and send to meowsic room

--- client/scenes/WaitingRoom.js
+++ client/scenes/WaitingRoom.js
@@ -1,116 +1,125 @@
 import Phaser from "phaser";
 
 export default class WaitingRoom extends Phaser.Scene {
-    constructor() {
-        super("WaitingRoom");
-        this.state = {};
-        this.hasBeenSet = false;
-    }
+  constructor() {
+    super("WaitingRoom");
+    this.state = {};
+    this.hasBeenSet = false;
+  }
 
-    init(data) {
-        //initializing the socket passed to the waiting room
-        this.socket = data.socket;
-        this.state.loggedInUser = data.user
-        // this.username = data.user.displayName
-    }
+  init(data) {
+    //initializing the socket passed to the waiting room
+    this.socket = data.socket;
+    this.state.loggedInUser = data.user;
+  }
 
+  preload() {
+    this.load.html("codeform", "assets/text/codeform.html");
+    this.load.image("waitingroom", "/assets/elements/room-code.png");
+    this.load.image("getroomcode", "/assets/elements/emptybox.png");
+  }
 
-    preload() {
-        this.load.html("codeform", "assets/text/codeform.html");
-        this.load.image("waitingroom", "/assets/elements/room-code.png")
-        this.load.image("getroomcode", "/assets/elements/emptybox.png")
-    }
+  create() {
+    const scene = this;
 
-    create() {
-        const scene = this;
+    scene.popUp = scene.add.graphics();
 
-        scene.popUp = scene.add.graphics();
+    console.log(
+      "waitingroom scene.state.loggedinuser",
+      scene.state.loggedInUser
+    );
 
-        console.log(scene.state.loggedInUser);
+    // for popup window
+    // scene.popUp.lineStyle(1, 0xffffff);
+    scene.popUp.fillStyle(0xfadfe6, 0.8);
+    // scene.popUp.strokeRect(50, 50, 1036, 540);
+    scene.popUp.fillRect(50, 50, 900, 550);
 
-        // for popup window
-        // scene.popUp.lineStyle(1, 0xffffff);
-        scene.popUp.fillStyle(0xfadfe6, 0.8);
-        // scene.popUp.strokeRect(50, 50, 1036, 540);
-        scene.popUp.fillRect(50, 50, 900, 550);
+    // for boxes
 
-        // for boxes
+    this.codeBoxes = this.add
+      .image(568, 320, "waitingroom")
+      .setOrigin(0.5, 0.5)
+      .setScale(0.37);
 
+    // //left popup
+    // scene.boxes.strokeRect(100, 200, 275, 100);
+    // scene.boxes = scene.add.graphics();
+    // scene.boxes.fillStyle(0xA873D1, .3);
+    // scene.boxes.fillRect(100, 200, 275, 100);
+    // scene.requestButton = scene.add.text(140, 215, "Request Room Key", {
+    //     fill: "#000000",
 
-        this.codeBoxes = this.add.image(568, 320, "waitingroom").setOrigin(0.5, 0.5).setScale(.37);
+    //     fontSize: "20px",
+    //     fontStyle: "bold",
+    // });
 
+    //right popup
+    // scene.boxes.strokeRect(425, 200, 275, 100);
+    // scene.boxes.fillRect(425, 200, 275, 100);
 
-        // //left popup
-        // scene.boxes.strokeRect(100, 200, 275, 100);
-        // scene.boxes = scene.add.graphics();
-        // scene.boxes.fillStyle(0xA873D1, .3);
-        // scene.boxes.fillRect(100, 200, 275, 100);
-        // scene.requestButton = scene.add.text(140, 215, "Request Room Key", {
-        //     fill: "#000000",
+    scene.inputElement = scene.add.dom(320, 420).createFromCache("codeform");
+    scene.inputElement.addListener("click");
+    scene.inputElement.on("click", function (event) {
+      if (event.target.name === "enterRoom") {
+        const input = scene.inputElement.getChildByName("code-form");
+        scene.socket.emit("isKeyValid", input.value);
+      }
+    });
+    this.getCodeBox = this.add.image(740, 380, "getroomcode");
+    scene.getCodeBox.setInteractive();
+    scene.getCodeBox.on("pointerdown", () => {
+      scene.socket.emit("getRoomCode");
+    });
 
-        //     fontSize: "20px",
-        //     fontStyle: "bold",
-        // });
+    scene.notValidText = scene.add.text(320, 470, "", {
+      fill: "#ff0000",
+      fontSize: "15px",
+    });
+    scene.roomKeyText = scene.add.text(720, 420, "", {
+      fill: "#00000",
+      fontSize: "50px",
+      // fontStyle: "bold",
+    });
 
-        //right popup
-        // scene.boxes.strokeRect(425, 200, 275, 100);
-        // scene.boxes.fillRect(425, 200, 275, 100);
+    // scene.socket.on('catPlayed', function (gameObject, dropZone, socketId) {
+    //     scene.socket.emit('catPlayed', gameObject, dropZone, socketId);
+    // });
 
-        scene.inputElement = scene.add.dom(320, 420).createFromCache("codeform");
-        scene.inputElement.addListener("click");
-        scene.inputElement.on("click", function (event) {
-            if (event.target.name === "enterRoom") {
-                const input = scene.inputElement.getChildByName("code-form");
-                scene.socket.emit("isKeyValid", input.value);
-            }
-        });
-        this.getCodeBox = this.add.image(740, 380, "getroomcode")
-        scene.getCodeBox.setInteractive();
-        scene.getCodeBox.on("pointerdown", () => {
-            scene.socket.emit("getRoomCode");
-        });
+    scene.socket.on("roomCreated", function (roomKey) {
+      scene.roomKey = roomKey;
+      scene.roomKeyText.setText(scene.roomKey);
+    });
 
+    scene.socket.on("keyNotValid", function () {
+      scene.notValidText.setText("Invalid Room Key");
+    });
+    scene.socket.on("keyIsValid", function (input) {
+      const username = scene.state.loggedInUser.username;
+      scene.socket.emit("joinRoom", input, username);
+      scene.scene.stop("WaitingRoom");
+      scene.scene.stop("MainScene");
+      scene.physics.pause();
+    });
 
-        scene.notValidText = scene.add.text(320, 470, "", {
-            fill: "#ff0000",
-            fontSize: "15px",
-        });
-        scene.roomKeyText = scene.add.text(720, 420, "", {
-            fill: "#00000",
-            fontSize: "50px",
-            // fontStyle: "bold",
-        });
+    //JOINED ROOM - SET STATE
+    scene.socket.on("setState", function (state) {
+      console.log("setstate state", state);
+      const { roomKey, players, usernames, numPlayers, placedCats } = state;
+      //   usernames.push(scene.state.loggedInUser.username);
 
-        // scene.socket.on('catPlayed', function (gameObject, dropZone, socketId) {
-        //     scene.socket.emit('catPlayed', gameObject, dropZone, socketId);
-        // });
-
-        scene.socket.on("roomCreated", function (roomKey) {
-            scene.roomKey = roomKey;
-            scene.roomKeyText.setText(scene.roomKey);
-        });
-
-        scene.socket.on("keyNotValid", function () {
-            scene.notValidText.setText("Invalid Room Key");
-        });
-        scene.socket.on("keyIsValid", function (input) {
-            scene.socket.emit("joinRoom", input);
-            scene.scene.stop("WaitingRoom");
-            scene.scene.stop('MainScene')
-            scene.physics.pause();
-        });
-        //JOINED ROOM - SET STATE
-        scene.socket.on("setState", function (state) {
-            const { roomKey, players, numPlayers, placedCats } = state;
-
-            scene.state.roomKey = roomKey;
-            scene.state.players = players;
-            scene.state.numPlayers = numPlayers;
-            scene.state.inRoom = true;
-            scene.state.placedCats = placedCats;
-            scene.scene.start("MeowsicRoom", { ...scene.state, socket: scene.socket, user: scene.state.loggedInUser });
-
-        })
-    }
-    update() { }
+      scene.state.roomKey = roomKey;
+      scene.state.players = players;
+      scene.state.usernames = usernames;
+      scene.state.numPlayers = numPlayers;
+      scene.state.inRoom = true;
+      scene.state.placedCats = placedCats;
+      scene.scene.start("MeowsicRoom", {
+        ...scene.state,
+        socket: scene.socket,
+        user: scene.state.loggedInUser,
+      });
+    });
+  }
+  update() {}
 }
